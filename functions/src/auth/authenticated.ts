410272783fee6873b0a1d60ecf0312d962746bcf
AJ-Sujit <sujit@ajency.in>
2019-09-30 09:56:50 +0530

    added authentication to apis

---
+++ functions/src/auth/authenticated.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import * as admin from 'firebase-admin';
+
+export async function isAuthenticated(req: Request, res: Response, next: Function) {
+   const { authorization } = req.headers
+
+   if (!authorization || !authorization.startsWith('Bearer'))
+       return res.status(401).send({ message: 'Unauthenticated' });
+
+   const split = authorization.split('Bearer ');
+   const token = split[1]
+
+   try {
+       const decodedToken: admin.auth.DecodedIdToken = await admin.auth().verifyIdToken(token);
+       console.log("decodedToken", JSON.stringify(decodedToken))
+       return next();
+   }
+   catch (err) {
+       console.error(`${err.code} -  ${err.message}`)
+       return res.status(401).send({ message: 'Unauthenticated' });
+   }
+}
--- functions/src/routes-config.ts
+++ functions/src/routes-config.ts
@@ -1,17 +1,21 @@
 import { Application } from "express";
 import Cart from "./controllers/CartController";
 import Address from "./controllers/AddressController";
+import { isAuthenticated } from "./auth/authenticated";
 
 export function routesConfig(app: Application) {
 	app.post('/add-to-cart',
+		isAuthenticated,
 		Cart.checkAvailability
 	);
 
 	app.post('/add-address',
+		isAuthenticated,
 		Address.addAddress
 	);
 
 	app.get('/get-addresses',
+		isAuthenticated,
 		Address.getAddresses
 	);
 }
