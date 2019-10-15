be323c1cb900576710cd99c7f4373e2387f4f894
AJ-Sujit <sujit@ajency.in>
2019-10-15 13:57:45 +0530

    removed swiggy’s api for reverse geocoding

--- functions/src/controllers/MiscController.ts
+++ functions/src/controllers/MiscController.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from "express";
 const axios = require('axios');
-
+const api_key = "AIzaSyAj9KpgYtINImiBr7lN4lHe8s1_wBFPzmo"
 let misc = {
 	placesAutoComplete : async (req : Request, res : Response) => {
-		let url = "https://maps.googleapis.com/maps/api/place/autocomplete/json?input="+ req.query.input +"&components=country:in&key=AIzaSyAj9KpgYtINImiBr7lN4lHe8s1_wBFPzmo"
+		let url = "https://maps.googleapis.com/maps/api/place/autocomplete/json?input="+ req.query.input +"&components=country:in&key=" + api_key;
 		axios.get(url)
 		  .then(function (response) {
 		    	return res.status(200).send(response.data);
@@ -16,7 +16,7 @@ let misc = {
 
 	reverseGeoCode : async (req : Request, res : Response) => {
 		if(req.query.place_id){
-			let url = "https://maps.googleapis.com/maps/api/place/details/json?place_id="+ req.query.place_id +"&fields=address_component,formatted_address,geometry,types&key=AIzaSyAj9KpgYtINImiBr7lN4lHe8s1_wBFPzmo"
+			let url = "https://maps.googleapis.com/maps/api/place/details/json?place_id="+ req.query.place_id +"&fields=address_component,formatted_address,geometry,types&key=" + api_key;
 			axios.get(url)
 			  .then(function (response) {
 			    	return res.status(200).send(response.data);
@@ -27,10 +27,10 @@ let misc = {
 			  })
 		}
 		else if(req.query.latlng){
-			let url = "https://www.swiggy.com/dapi/misc/reverse-geocode?latlng=" + req.query.latlng;
+			// let url = "https://www.swiggy.com/dapi/misc/reverse-geocode?latlng=" + req.query.latlng;
+			let url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + req.query.latlng +"&key=" + api_key;;
 			axios.get(url)
 			  .then(function (response) {
-			  		// console.log("response from swiggy api=>", response.data);
 			    	return res.status(200).send(response.data);
 			  })
 			  .catch(function (error) {
