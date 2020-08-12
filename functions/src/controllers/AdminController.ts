83ab624b46c3cc261e6711f3c717e9c16f3e7638
latesh <latesh@ajency.in>
2020-08-12 11:24:37 +0530

    create user order object payment object

--- functions/src/controllers/AdminController.ts
+++ functions/src/controllers/AdminController.ts
@@ -3,6 +3,7 @@ import * as admin from 'firebase-admin';
 const { AsyncParser, parse } = require('json2csv');
 import * as Airtable from 'airtable';
 import { extractInstanceAndPath } from "firebase-functions/lib/providers/database";
+import e = require("express");
 const { _ } = require("underscore")
 const cred = require('../../credentials.json');
 Airtable.configure({
@@ -15,10 +16,11 @@ const EXTERNAL_ORDERS_MANDATORY_FIELDS = [
     "variant_id",
     "product_id",
     "order_date",
-    "order_type",
+    "external_order_type",
     "payment_mode",
     "mrp",
-    "sale_price"
+    "sale_price",
+    "quantity"
 ]
 const Admin = {
     getProductsCSV: async function (req: Request, res: Response) {
@@ -275,7 +277,8 @@ const Admin = {
 
                 records.forEach(async function (record) {
                     let recordFieldKeys = Object.keys(record.fields)
-                    const checkDifference = _.difference(EXTERNAL_ORDERS_MANDATORY_FIELDS, recordFieldKeys)
+                    const checkDifference = _.difference(recordFieldKeys, EXTERNAL_ORDERS_MANDATORY_FIELDS)
+
                     if (!checkDifference.length) {
                         const orderObj = record.fields
                         if (dataMaster[orderObj.order_id]) {
@@ -290,6 +293,9 @@ const Admin = {
                             productMaster[orderObj.product_id] = []
                             productMaster[orderObj.product_id].push({ variant_id: orderObj.variant_id })
                         }
+                    } else {
+                        console.log("validation fale");
+
                     }
 
                 });
@@ -298,15 +304,9 @@ const Admin = {
 
             }, async function done(err) {
                 if (err) { console.error(err); return; }
-                console.log(dataMaster)
-                let phoneDir = []
-                for (const key in dataMaster) {
-                    let phoneNos = _.pluck(dataMaster[key], "phone");
-                    phoneNos = _.uniq(phoneNos)
-                    phoneDir = _.union(phoneDir, phoneNos)
-                }
-                console.log(phoneDir)
                 for (const productId in productMaster) {
+                    console.log(productId);
+
                     try {
                         const prodRef = await db.collection('products').doc(productId).get()
                         if (prodRef.exists) {
@@ -315,67 +315,224 @@ const Admin = {
                     } catch (e) {
                         console.log(e)
                     }
-                    admin.auth().createUser({
-                        phoneNumber: '+919767992594'
-                    })
-                        .then(function (userRecord) {
-                            // See the UserRecord reference doc for the contents of userRecord.
-                            console.log('Successfully created new user:', userRecord.uid);
-                        })
-                        .catch(function (error) {
-                            console.log('Error creating new user:', error);
-                        });
                 }
-                res.status(200).send({ phoneDir })
+                //build order object
+
+                const ordermaster = []
+                console.log(Object.keys(dataMaster).length);
+                const userStore = {}
+                for (let ex_order_id in dataMaster) {
+                    const orderObj = {
+                        airtableUpdated: true,
+                        userNotified: true,
+                        externalOrderType: "",
+                        externalOrderId: "",
+                        business_id: business_id,
+                        cart_count: 0,
+                        order_id: "",
+                        items: [],
+                        shipping_address: {
+                            landmark: "Flat number, Building name not entered as part of the order.",
+                            formatted_address: "Google location not entered as part of the order",
+                            lat_long: [15.4873766, 73.83266669999999],
+                            phone: "",
+                            name: "",
+                            email: ""
+
+                        },
+                        order_mode: "manual",
+                        order_no: "",
+                        payment_id: "",
+                        status: "placed",
+                        stock_location_id: "",
+                        summary: {
+                            cart_discount: 0,
+                            mrp_total: 0,
+                            sale_price_total: 0,
+                            shipping_fee: 0,
+                            you_pay: 0
+                        },
+                        timestamp: "",
+                        token: 0,
+                        user_id: ""
+                    }
+                    const paymentObj = {
+                        order_id: "",
+                        order_details:"",
+                        status:"captured",
+                        timestamp:"",
+                        user_id:""
+                    }
+                    try {
+                        // build items
+                        let firebase_order_id = ""
+                        let phone_number = ""
+                        if (!phone_number) {
+                            let phoneNos = _.pluck(dataMaster[ex_order_id], "phone");
+                            phoneNos = _.uniq(phoneNos)
+                            console.log(phoneNos);
+                            
+                            phone_number = phoneNos[0].replace("+91", "");
+                        }
+                        orderObj.shipping_address.phone = phone_number;
+                        let mrp_total = 0, sale_price_total = 0, cart_discount = 0, delivery_fee = 0;
+                        for (const index in dataMaster[ex_order_id]) {
+                            mrp_total = mrp_total + (dataMaster[ex_order_id][index].mrp * dataMaster[ex_order_id][index].quantity);
+                            sale_price_total = sale_price_total + (dataMaster[ex_order_id][index].sale_price * dataMaster[ex_order_id][index].quantity);
+                            if (!firebase_order_id) {
+                                firebase_order_id = dataMaster[ex_order_id][index].external_order_type + '_' + dataMaster[ex_order_id][index].order_id;
+                                orderObj.order_id = paymentObj.order_id = firebase_order_id    
+                                orderObj.payment_id = "payment_"+dataMaster[ex_order_id][index].external_order_type + '_' + dataMaster[ex_order_id][index].order_id
+                            }
+                            if (!orderObj.timestamp) {
+                                orderObj.timestamp = paymentObj.timestamp = dataMaster[ex_order_id][index].order_date
+                            }
+
+                            if (!paymentObj.order_details) {
+                                paymentObj.order_details = `{method:${dataMaster[ex_order_id][index].payment_mode}}`
+                            }
+
+
+                            const product = productMaster[dataMaster[ex_order_id][index].product_id]
+
+                            const variant = product.variants.find((variant => variant.id == dataMaster[ex_order_id][index].variant_id))
+                            const item = {
+                                day: variant.day,
+                                description: product.description,
+                                mrp: dataMaster[ex_order_id][index].mrp,
+                                product_id: dataMaster[ex_order_id][index].product_id,
+                                product_name: product.title,
+                                quantity: dataMaster[ex_order_id][index].quantity,
+                                sale_price: dataMaster[ex_order_id][index].sale_price,
+                                size: variant.size,
+                                slot: variant.slot,
+                                timestamp: dataMaster[ex_order_id][index].order_date,
+                                variant_id: variant.id,
+                                veg: product.veg
+
+                            }
+                            orderObj.items.push(item)
+                        }
+                        orderObj.summary.mrp_total = mrp_total
+                        orderObj.summary.sale_price_total = sale_price_total
+                        orderObj.summary.you_pay = (sale_price_total - cart_discount) + delivery_fee;
+                        let userObj:any = {}
+                        if(userStore[phone_number]) {
+                            userObj = userStore[phone_number]
+                        } else {
+                            const newPhone = "+91"+phone_number
+                            userObj = await Admin.getUser(newPhone)
+                            userStore[phone_number] = userObj
+                        }
+                        orderObj.shipping_address.name = userObj.name
+                        orderObj.shipping_address.email = userObj.email
+                        orderObj.user_id = userObj.id
+
+                        // create Payment object
+                        paymentObj.user_id = userObj.id
+                    } catch (error) {
+                        console.log(error);
+                        res.status(500).send({ error })
+                    }
+                    Admin.saveOrdersUnderUser(orderObj, paymentObj, userStore[orderObj.shipping_address.phone])
+                    ordermaster.push(orderObj)
+                }
+
+
+                res.status(200).send({ ordermaster:ordermaster[0] })
             });
         } catch (e) {
             res.status(500).send({ message: "Something went wrong" })
         }
     },
-    
-    checkUserExist: async (phone) => {
-        const firestore = admin.firestore(); 
-		const userDocs = await firestore.collection('user-details').where('phone', "==", phone).get();
-        console.log(userDocs.size,"checkInDb===>");
-        const size = userDocs.size
-        if(size) {
-			let oldUser = false
-			for(let i =0; i<size; i++) {
-                const userData = userDocs[i].data()
-                if(userData.verified) {
-                    return true;
-                    break;
-                } else {
 
+    getUser: (phone) => {
+        console.log("checkInDb===>");
+        return new Promise(async (resolve, reject) => {
+
+            const firestore = admin.firestore();
+            const userDocs = await firestore.collection('user-details').where('phone', "==", phone.replace("+91","")).get();
+            const size = userDocs.size
+            
+            if (size) {
+                let userObj:any = false
+                for (let i = 0; i < size; i++) {
+                    const userData = userDocs.docs[i].data()
+                    
+                    if (userData.verified) {
+                        userObj ={id: userDocs.docs[i].id, ...userData} 
+                        break;
+                    }
+                }
+                if (userObj) {
+                    resolve(userObj)
+                } else {
+                    resolve({id: userDocs.docs[0].id, ...userDocs[0].data()} )
+                }
+            } else {
+                try {
+                    
+                    let newUser  = await Admin.createVerifiedUser(phone)
+                    resolve(newUser)
+                } catch (error) {
+                    reject(false)
                 }
             }
-        }
-        return true
+        })
     },
-    createUser:  function(phone) {
-        return new Promise(async (resolve,reject) => {
+    createVerifiedUser: function (phone) {
+        console.log("create user", phone);
+        
+        return new Promise(async (resolve, reject) => {
             try {
                 const newUser = await admin.auth().createUser({
                     phoneNumber: phone
                 })
 
+                await admin.firestore().collection("user-details").doc(newUser.uid).set({
+                    verified: true,
+                    phone: phone.replace("+91", "")
+                })
+                resolve({
+                    id: newUser.uid,
+                    verified: true,
+                    phone: phone.replace("+91", "")
+                })
             } catch (error) {
-               console.log(error);
-                
+                console.log(error);
+                reject(false)
             }
         })
-       
-            .then(function (userRecord) {
-                // See the UserRecord reference doc for the contents of userRecord.
-                console.log('Successfully created new user:', userRecord.uid);
-               return userRecord
-
-
+    },
+    saveOrdersUnderUser:(orderObj:any, paymentObj:any, userObj) => {
+        console.log(`saving ${orderObj.order_id} for user ${userObj.phone}`, orderObj);
+        
+        admin.firestore().collection('user-details').doc(userObj.id)
+        .collection('orders')
+        .doc(orderObj.order_id)
+        .set(orderObj).then(res => {
+            admin.firestore().collection('payments').doc(orderObj.payment_id).set(paymentObj).then((res) => {
+                console.log("added payment");
+            }).catch(error => {
+                console.log(error);
             })
-            .catch(function (error) {
-                console.log('Error creating new user:', error);
-                return error
-            });
+            admin.firestore().collection('user-orders-map').doc().set({user_id:userObj.id, order_id: orderObj.order_id}).then((res) => {
+                console.log("added payment");
+            }).catch(error => {
+                console.log(error);
+            })
+        }).catch(error => {
+            console.log(error);
+            
+        })
+        return true
+    },
+    contains: function (target, pattern) {
+        var value = 0;
+        pattern.forEach(function (word) {
+            value = value + target.includes(word);
+        });
+        return (value === 1)
     }
 
 }
